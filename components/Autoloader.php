5edd3b7cd9939445c0b723971259ec5dbf9eee7c
Erzikov <lesha-erzikov@mail.ru>
2018-07-27 21:19:25 +0300

    Update Autoloader

--- components/Autoloader.php
+++ components/Autoloader.php
@@ -1,4 +1,5 @@
 <?php 
-spl_autoload_extensions(".php");
-spl_autoload_register();  
-
+spl_autoload_register(function($className) {
+    $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+    include_once $_SERVER['DOCUMENT_ROOT'] .'/' . $className . '.php';
+});
