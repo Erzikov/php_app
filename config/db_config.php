d4919d2faa710644269b7281ba8f7ff91580d30d
Erzikov <lesha-erzikov@mail.ru>
2018-07-27 22:20:43 +0300

    Update db_config

--- config/db_config.php
+++ config/db_config.php
@@ -4,7 +4,7 @@ return array(
     'dbname' => 'magaz',
     'charset' => 'utf8',
     'user' => 'root',
-    'password' => '',
+    'password' => '1111',
     'options' => [PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
 			  	  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 			 	  PDO::ATTR_EMULATE_PREPARES   => false]
